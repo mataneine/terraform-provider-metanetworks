91ceb5d202a82fc67229ae6a9e7eb17ddd2e2577
Matan Eine <matan.eine@gmail.com>
2020-11-22 20:53:53 +0200

    rename modifyUsers to updateGroupUsers

--- metanetworks/group.go
+++ metanetworks/group.go
@@ -111,8 +111,7 @@ func (c *Client) DeleteGroup(groupID string) error {
 	return nil
 }
 
-// modifyUsers ...
-func (c *Client) modifyUsers(groupID string, users []string, operation string) (*Group, error) {
+func (c *Client) updateGroupUsers(groupID string, users []string, operation string) (*Group, error) {
 	jsonData, err := json.Marshal(users)
 	if err != nil {
 		return nil, err
@@ -134,12 +133,12 @@ func (c *Client) modifyUsers(groupID string, users []string, operation string) (
 
 // AddGroupUsers ...
 func (c *Client) AddGroupUsers(groupID string, users []string) (*Group, error) {
-	return c.modifyUsers(groupID, users, "add_users")
+	return c.updateGroupUsers(groupID, users, "add_users")
 }
 
 // RemoveGroupUsers ...
 func (c *Client) RemoveGroupUsers(groupID string, users []string) (*Group, error) {
-	return c.modifyUsers(groupID, users, "remove_users")
+	return c.updateGroupUsers(groupID, users, "remove_users")
 }
 
 // SetGroupRoles ...
