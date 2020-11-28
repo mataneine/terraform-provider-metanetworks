a087fe488bda1843b45ea9ee190fef135fd33c34
Matan Eine <matan.eine@gmail.com>
2020-11-28 21:49:40 +0200

    add metanetworks/resource_native_service_alias.go

--- metanetworks/provider.go
+++ metanetworks/provider.go
@@ -45,6 +45,7 @@ func Provider() *schema.Provider {
 			"metanetworks_metaport_attachment":          resourceMetaportAttachment(),
 			"metanetworks_metaport_otac":                resourceMetaportOTAC(),
 			"metanetworks_metaport":                     resourceMetaport(),
+			"metanetworks_native_service_alias":         resourceNativeServiceAlias(),
 			"metanetworks_native_service":               resourceNativeService(),
 			"metanetworks_policy":                       resourcePolicy(),
 			"metanetworks_protocol_group":               resourceProtocolGroup(),
